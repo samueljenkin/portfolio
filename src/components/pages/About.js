e7908a83e89bc9315bad2043098af3714391487e
Sam26 <samueltjenkin@gmail.com>
2023-07-05 14:44:40 +0930

    fix styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -32,62 +32,61 @@ const About = () => {
           )}
         </section>
       </section>
-      
 
       <section className="interest-list">
         <h2>Interests</h2>
         <sections className="interests">
           <section className="interest">
-            <section className="title">
+            <section className="heading">
               <span class="material-symbols-outlined">landscape</span> 
               <h3>Great Outdoors</h3>
             </section> 
             <p>I enjoy a range of outdoor activities such as hiking, camping, swimming, snowboarding, and rock climbing. These pursuits allow me to connect with nature and challenge myself physically while experiencing the beauty of the outdoors.</p>
           </section>
           <section className="interest">
-            <section className="title">
+            <section className="heading">
               <span class="material-symbols-outlined">code</span> 
               <h3>Coding Challenges</h3>
             </section> 
             <p>I actively seek out coding challenges to enhance my problem-solving skills. Engaging in these challenges keeps me up-to-date with the latest trends and techniques in programming, fostering continuous growth in my abilities.</p>
           </section>
           <section className="interest">
-            <section className="title">
-              <span class="material-symbols-outlined">sports</span> 
+            <section className="heading">
+              <span class="material-symbols-outlined">sports_basketball</span> 
               <h3>Sports</h3>
             </section> 
             <p>I am passionate about sports, particularly football and basketball. Participating in these activities not only keeps me physically fit but also cultivates teamwork and leadership skills.</p>
           </section>
           <section className="interest">
-            <section className="title">
+            <section className="heading">
               <span class="material-symbols-outlined">flight</span> 
               <h3>Travel</h3>
             </section> 
             <p>I have a deep love for travel, exploring both Australia and the world. It provides me with opportunities to immerse myself in different cultures, broaden my horizons, and create lasting memories.</p>
           </section>
           <section className="interest">
-            <section className="title">
+            <section className="heading">
               <span class="material-symbols-outlined">restaurant</span> 
               <h3>Culinary Exploration</h3>
             </section> 
             <p>I enjoy diverse food experiences through dining out and cooking at home. Exploring various cuisines allows me to embrace cultural diversity and express my creativity in the kitchen.</p>
           </section>
           <section className="interest">
-            <section className="title">
+            <section className="heading">
               <span class="material-symbols-outlined">music_note</span> 
               <h3>Music</h3>
             </section> 
             <p>I have a deep passion for music and enjoy exploring various genres. Whether attending live performances or discovering new artists, music is a constant source of inspiration and relaxation in my life.</p>
           </section>
           <section className="interest">
-            <section className="title">
-              <span class="material-symbols-outlined">landscape</span> 
+            <section className="heading">
+              <span class="material-symbols-outlined">diversity_3</span> 
               <h3>Quality Time</h3>
             </section> 
             <p>Spending cherished moments with family and friends is important to me. I value meaningful connections and make an effort to create lasting memories through shared experiences.</p>
           </section>
           <section className="interest">
-            <section className="title">
+            <section className="heading">
               <span class="material-symbols-outlined">weekend</span> 
               <h3>Relaxation</h3>
             </section> 
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,7 @@
 import DefaultLayout from "../layout/DefaultLayout";
 import React, { useState } from "react";
-import { TextField, Typography, Card, CardContent, Grid, Button } from "@mui/material";
+import { TextField, Button } from "@mui/material";
+import './Contact.css'
 
 const FORM_ENDPOINT = "https://public.herotofu.com/v1/c9d84e70-196d-11ee-b0a7-9f000c4c1540"; 
 
@@ -44,18 +45,10 @@ const Contact = () => {
 
   if (submitted) {
     return (
-      <Card
-        style={{
-          maxWidth: 450,
-          margin: '0 auto',
-          padding: '20px 5px',
-          marginTop: '3.5rem',
-          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)'
-        }}
-      >
-        <CardContent sx={{ marginBottom: 0 }}>
-          <Typography gutterBottom variant='h3' sx={{ textAlign: 'center', marginBottom: '2rem' }}>Submitted!</Typography>
-          <Typography color='textSecondary' variant="body2" component='p'>
+      <DefaultLayout>
+        <section className="card">
+          <h2>Submitted!</h2>
+          <p>
             Dear {sender},
             <br /><br />
             Thank you for reaching out and sending your message! I appreciate your interest and wanted to express my gratitude for your time and effort. I will review your message carefully and get back to you promptly. If you have any further questions or need additional information, please feel free to let me know. 
@@ -65,55 +58,66 @@ const Contact = () => {
             Best regards,
             <br /><br />
             Sam Jenkin
-          </Typography>
-        </CardContent>
-      </Card>
+          </p>
+        </section>
+      </DefaultLayout>
     );
   }
 
   return (
-    <>
-      <Card
-        style={{
-          maxWidth: 450,
-          margin: '0 auto',
-          padding: '20px 5px',
-          marginTop: '3.5rem',
-          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)'
-        }}
-      >
-        <CardContent>
-          <Typography gutterBottom variant='h3' sx={{ textAlign: 'center' }}>Contact Me</Typography>
-          <Typography color='textSecondary' variant="body2" component='p' sx={{ textAlign: 'center', marginBottom: '1rem' }}>
-            I will get back to you as soon as I can!
-          </Typography>
+    <DefaultLayout>
+      <section className="card">
+        <section className="contact-heading">
+          <h1>Contact Me</h1>
+          <p>I will get back to you as soon as I can!</p>
+        </section>
+        
+        <form
+          action={FORM_ENDPOINT}
+          onSubmit={handleSubmit}
+          method="POST"
+          accept-charset="UTF-8"
+        >
+          <TextField
+            placeholder="Enter name"
+            name="name"
+            variant="outlined"
+            fullWidth
+            required
+          />
+          <TextField
+            type="email"
+            placeholder="Enter email" 
+            name="email" 
+            variant="outlined" 
+            fullWidth 
+            required
+          />
+          <TextField 
+            multiline 
+            rows={4} 
+            placeholder="Type your message here" 
+            name="message" 
+            variant="outlined" 
+            fullWidth 
+            required
+          />
+          <Button 
+            type="submit" 
+            variant='contained' 
+            color='primary' 
+            fullWidth
+          >
+            Submit
+          </Button>
+        </form>
+      </section>
 
-          <form action={FORM_ENDPOINT} onSubmit={handleSubmit} method="POST" accept-charset="UTF-8">
-            <Grid container spacing={1}>
-              <Grid xs={12} item>
-                <TextField placeholder="Enter name" name="name" variant="outlined" fullWidth required />
-              </Grid>
-
-              <Grid xs={12} item>
-                <TextField type="email" placeholder="Enter email" name="email" variant="outlined" fullWidth required />
-              </Grid>
-
-              <Grid xs={12} item>
-                <TextField multiline rows={4} placeholder="Type your message here" name="message" variant="outlined" fullWidth required/>
-              </Grid>
-
-              <Grid xs={12} item>
-                <Button type="submit" variant='contained' color='primary' fullWidth>Submit</Button>
-              </Grid>
-            </Grid>
-          </form>
-        </CardContent>
-      </Card>
-
+      {/* catch hackers */}
       <div style={{ textIndent: '-99999px', whiteSpace: 'nowrap', overflow: 'hidden', position: 'absolute' }}  aria-hidden="true">
         <input type="text" name="_gotcha" tabindex="-1" autocomplete="off" />
       </div>
-    </>
+    </DefaultLayout>
   )
 }
 
